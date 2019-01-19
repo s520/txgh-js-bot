21a9f573f841dcc19744dc5f3bfc1a39df73eeff
S520 <29241703+s520@users.noreply.github.com>
2019-01-19 16:01:08 +0900

    Fix: deleted redundant code

--- index.js
+++ index.js
@@ -23,153 +23,139 @@ const extFilter = new RegExp("\\" + TX_RESOURCE_EXT);
 const pathToSlug = new RegExp("/|\\.", "g");
 
 function AddModResources(app, commits) {
-    return new Promise((resolve, reject) => {
-        var addModResources = {};
-        for (let commit of commits) {
-            app.log("processing commit");
-            for (let file of commit.added) {
-                app.log("processing added file: " + file);
-                if (file.match(fileFilter) && file.match(extFilter)) {
-                    addModResources[file] = commit.tree_id;
-                }
-            }
-            for (let file of commit.modified) {
-                app.log("processing modified file: " + file);
-                if (file.match(fileFilter) && file.match(extFilter)) {
-                    addModResources[file] = commit.tree_id;
-                }
+    var addModResources = {};
+    for (let commit of commits) {
+        app.log("processing commit");
+        for (let file of commit.added) {
+            app.log("processing added file: " + file);
+            if (file.match(fileFilter) && file.match(extFilter)) {
+                addModResources[file] = commit.tree_id;
             }
         }
-        resolve(addModResources);
-    });
-}
-
-function UpdateResources(app, githubApi, repoOwner, repoName, resources) {
-    return new Promise(async (resolve, reject) => {
-        for (let resourcePath of Object.keys(resources)) {
-            app.log("process updated resource");
-            const tree = await githubApi.gitdata.getTree({
-                owner: repoOwner,
-                repo: repoName,
-                tree_sha: resources[resourcePath],
-                recursive: 1
-            });
-            for (let file of tree.data.tree) {
-                app.log("process each tree entry: " + file.path);
-                if (file.path == resourcePath) {
-                    app.log("process resource file: " + resourcePath);
-                    const blob = await githubApi.gitdata.getBlob({
-                        owner: repoOwner,
-                        repo: repoName,
-                        file_sha: file.sha
-                    });
-                    var content = new Buffer(blob.data.content, blob.data.encoding).toString();
-                    await UploadResource(resourcePath, content);
-                    app.log("updated tx_resource: " + resourcePath);
-                }
+        for (let file of commit.modified) {
+            app.log("processing modified file: " + file);
+            if (file.match(fileFilter) && file.match(extFilter)) {
+                addModResources[file] = commit.tree_id;
             }
         }
-        resolve();
-    });
+    }
+    return addModResources
 }
 
-function GenarateResourceSlug(resourcePath) {
-    return new Promise(async (resolve, reject) => {
-        const resource = await stringReplaceAsync(resourcePath, fileFilter, "");
-        const resourceSlug = await stringReplaceAsync(resource, pathToSlug, "-");
-        resolve(resourceSlug);
-    });
+async function UpdateResources(app, githubApi, repoOwner, repoName, resources) {
+    for (let resourcePath of Object.keys(resources)) {
+        app.log("process updated resource");
+        const tree = await githubApi.gitdata.getTree({
+            owner: repoOwner,
+            repo: repoName,
+            tree_sha: resources[resourcePath],
+            recursive: 1
+        });
+        for (let file of tree.data.tree) {
+            app.log("process each tree entry: " + file.path);
+            if (file.path == resourcePath) {
+                app.log("process resource file: " + resourcePath);
+                const blob = await githubApi.gitdata.getBlob({
+                    owner: repoOwner,
+                    repo: repoName,
+                    file_sha: file.sha
+                });
+                var content = new Buffer(blob.data.content, blob.data.encoding).toString();
+                await UploadResource(resourcePath, content);
+                app.log("updated tx_resource: " + resourcePath);
+            }
+        }
+    }
+    return;
 }
 
-function UploadResource(resourcePath, content) {
-    return new Promise(async (resolve, reject) => {
-        const resourceSlug = await GenarateResourceSlug(resourcePath);
-        try {
-            await txApi.resource(TX_PROJECT_SLUG, resourceSlug);
-            await txApi.resourceSourceStringsUpdate(TX_PROJECT_SLUG, resourceSlug, {
-                content: content
-            });
-        } catch (error) {
+async function GenarateResourceSlug(resourcePath) {
+    const resource = await stringReplaceAsync(resourcePath, fileFilter, "");
+    const resourceSlug = await stringReplaceAsync(resource, pathToSlug, "-");
+    return (resourceSlug);
+}
+
+async function UploadResource(resourcePath, content) {
+    const resourceSlug = await GenarateResourceSlug(resourcePath);
+    await txApi.resource(TX_PROJECT_SLUG, resourceSlug)
+        .catch(async () => {
             await txApi.resourceCreate(TX_PROJECT_SLUG, {
                 slug: resourceSlug,
                 name: resourcePath,
                 i18n_type: TX_RESOURCE_TYPE,
                 content: content
-            })
-        }
-        resolve();
+            });
+            return;
+        })
+    await txApi.resourceSourceStringsUpdate(TX_PROJECT_SLUG, resourceSlug, {
+        content: content
     });
+    return;
 }
 
-function AllResources(app, githubApi, repoOwner, repoName, headTreeSha) {
-    return new Promise(async (resolve, reject) => {
-        var allResources = {};
-        const tree = await githubApi.gitdata.getTree({
-            owner: repoOwner,
-            repo: repoName,
-            tree_sha: headTreeSha,
-            recursive: 1
-        });
-        for (let file of tree.data.tree) {
-            app.log("process each tree entry: " + file.path);
-            if (file.path.match(fileFilter) && file.path.match(extFilter)) {
-                allResources[file.path] = await GenarateResourceSlug(file.path);
+async function AllResources(app, githubApi, repoOwner, repoName, headTreeSha) {
+    var allResources = {};
+    const tree = await githubApi.gitdata.getTree({
+        owner: repoOwner,
+        repo: repoName,
+        tree_sha: headTreeSha,
+        recursive: 1
+    });
+    for (let file of tree.data.tree) {
+        app.log("process each tree entry: " + file.path);
+        if (file.path.match(fileFilter) && file.path.match(extFilter)) {
+            allResources[file.path] = await GenarateResourceSlug(file.path);
+        }
+    }
+    return (allResources);
+}
+
+async function AllTranslations(app, resources) {
+    var allTranslations = {};
+    app.log("process get all translations");
+    for (let resourcePath of Object.keys(resources)) {
+        app.log("process get translations: " + resourcePath);
+        for (let lang of TX_TARGET_LANG) {
+            app.log("process get translations: " + lang);
+            if (lang != TX_RESOURCE_LANG) {
+                const result = await txApi.translation(TX_PROJECT_SLUG, resources[resourcePath], lang);
+                allTranslations[resourcePath.replace(fileFilter, TX_TARGET_PATH.replace(new RegExp("<lang>", "g"), lang))] = result.data;
+                app.log("got translation:" + resourcePath);
             }
         }
-        resolve(allResources);
-    });
+    }
+    return (allTranslations);
 }
 
-function AllTranslations(app, resources) {
-    return new Promise(async (resolve, reject) => {
-        var allTranslations = {};
-        app.log("process get all translations");
-        for (let resourcePath of Object.keys(resources)) {
-            app.log("process get translations: " + resourcePath);
-            for (let lang of TX_TARGET_LANG) {
-                app.log("process get translations: " + lang);
-                if (lang != TX_RESOURCE_LANG) {
-                    const result = await txApi.translation(TX_PROJECT_SLUG, resources[resourcePath], lang);
-                    allTranslations[resourcePath.replace(fileFilter, TX_TARGET_PATH.replace(new RegExp("<lang>", "g"), lang))] = result.data;
-                    app.log("got translation:" + resourcePath);
-                }
-            }
-        }
-        resolve(allTranslations);
+async function CommitTranslations(app, githubApi, repoOwner, repoName, headSha, headTreeSha, translations) {
+    app.log("process commit all translations");
+    const tree = await githubApi.gitdata.createTree({
+        owner: repoOwner,
+        repo: repoName,
+        base_tree: headTreeSha,
+        tree: Object.keys(translations).map(path => {
+            return {
+                path,
+                mode: "100644",
+                content: translations[path]
+            };
+        })
     });
-}
-
-function CommitTranslations(app, githubApi, repoOwner, repoName, headSha, headTreeSha, translations) {
-    return new Promise(async (resolve, reject) => {
-        app.log("process commit all translations");
-        const tree = await githubApi.gitdata.createTree({
-            owner: repoOwner,
-            repo: repoName,
-            base_tree: headTreeSha,
-            tree: Object.keys(translations).map(path => {
-                return {
-                    path,
-                    mode: '100644',
-                    content: translations[path]
-                };
-            })
-        });
-        const commit = await githubApi.gitdata.createCommit({
-            owner: repoOwner,
-            repo: repoName,
-            message: "Update translations from transifex",
-            tree: tree.data.sha,
-            parents: [headSha]
-        });
-        await githubApi.gitdata.updateRef({
-            owner: repoOwner,
-            repo: repoName,
-            ref: "heads/" + GITHUB_BRANCH,
-            sha: commit.data.sha
-        });
-        app.log("commited all translations");
-        resolve();
+    const commit = await githubApi.gitdata.createCommit({
+        owner: repoOwner,
+        repo: repoName,
+        message: "Update translations from transifex",
+        tree: tree.data.sha,
+        parents: [headSha]
     });
+    await githubApi.gitdata.updateRef({
+        owner: repoOwner,
+        repo: repoName,
+        ref: "heads/" + GITHUB_BRANCH,
+        sha: commit.data.sha
+    });
+    app.log("commited all translations");
+    return;
 }
 
 module.exports = app => {
@@ -186,7 +172,7 @@ module.exports = app => {
         }
         var repoOwner = context.payload.repository.owner.name;
         var repoName = context.payload.repository.name;
-        const addModResources = await AddModResources(app, context.payload.commits);
+        const addModResources = AddModResources(app, context.payload.commits);
         await UpdateResources(app, context.github, repoOwner, repoName, addModResources);
         const allResources = await AllResources(app, context.github, repoOwner, repoName, context.payload.head_commit.tree_id);
         const allTranslations = await AllTranslations(app, allResources);

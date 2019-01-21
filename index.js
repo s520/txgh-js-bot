e6adf7b83acbc6eb94ae153ee7eec0f11ac1a603
S520 <29241703+s520@users.noreply.github.com>
2019-01-22 03:16:49 +0900

    Change: enabled to receive Transifex's Webhook

--- index.js
+++ index.js
@@ -12,13 +12,17 @@ const TX_RESOURCE_EXT = process.env.TX_RESOURCE_EXT;
 const TX_ALL_UPDATE = process.env.TX_ALL_UPDATE || false;
 const TX_TARGET_PATH = process.env.TX_TARGET_PATH;
 const TX_WEBHOOK_PATH = "/transifex";
-const TX_WEBHOOK_PROXY_URL = process.env.TX_WEBHOOK_PROXY_URL;
+const GITHUB_INSTALL_ID = process.env.GITHUB_INSTALL_ID;
+const [GITHUB_REPO_OWNER, GITHUB_REPO_NAME] = process.env.GITHUB_REPO.split("/");
 const GITHUB_BRANCH = process.env.GITHUB_BRANCH;
 const GITHUB_WEBHOOK_PATH = "/github";
 const GITHUB_WEBHOOK_PROXY_URL = process.env.GITHUB_WEBHOOK_PROXY_URL;
 const PORT = process.env.PORT || 3000;
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET || "development";
 
 const fs = require("fs");
+const md5 = require("md5");
+const crypto = require("crypto");
 const stringReplaceAsync = require("string-replace-async");
 
 const Logger = require("bunyan");
@@ -38,7 +42,7 @@ const app = new GitHubApp({
 
 const GitHubWebhooksApi = require("@octokit/webhooks");
 const githubWebhooks = new GitHubWebhooksApi({
-    secret: process.env.WEBHOOK_SECRET || "development",
+    secret: WEBHOOK_SECRET,
     path: GITHUB_WEBHOOK_PATH
 });
 
@@ -66,6 +70,29 @@ const createWebhookProxy = (url, path) => {
 const express = require("express");
 const server = express();
 server.use(githubWebhooks.middleware);
+
+const bodyParser = require("body-parser");
+server.use(TX_WEBHOOK_PATH, bodyParser.raw({ type: "application/json" }));
+
+/**
+ * TransifexのWebhookを検証する関数
+ * @param {Object} req - Webhookへのリクエスト
+ * @param {string} secret - Webhookの秘密鍵
+ * @returns {boolean} 検証結果
+ */
+const VerifyTxWebhook = (req, secret) => {
+    const actualSig = req.headers["x-tx-signature-v2"];
+    const url = req.headers["x-tx-url"];
+    const date = req.headers.date;
+    const data = Buffer.from(req.body).toString();
+    logger.info("Actual signature: " + actualSig);
+    const contentMd5 = md5(data);
+    const msg = ["POST", url, date, contentMd5].join("\n");
+    const hmac = crypto.createHmac("sha256", secret);
+    const expectedSig = hmac.update(msg).digest().toString("base64");
+    logger.info("Expected signature: " + expectedSig);
+    return actualSig == expectedSig;
+};
 
 const fileFilter = new RegExp(TX_RESOURCE_REG.replace(new RegExp("<lang>", "g"), TX_RESOURCE_LANG));
 const extFilter = new RegExp("\\" + TX_RESOURCE_EXT);
@@ -326,67 +353,142 @@ githubWebhooks.on("push", async event => {
     if (!branch.match(branchFilter)) {
         return;
     }
-    const github = await app.asInstallation(event.payload.installation.id);
-    const repoOwner = event.payload.repository.owner.name;
-    const repoName = event.payload.repository.name;
+    if (event.payload.installation.id != GITHUB_INSTALL_ID ||
+        event.payload.repository.owner.name != GITHUB_REPO_OWNER ||
+        event.payload.repository.name != GITHUB_REPO_NAME) {
+        throw new Error("Environment variable setting is wrong.");
+    }
+    const github = await app.asInstallation(GITHUB_INSTALL_ID);
     const headSha = event.payload.head_commit.id;
     const headTreeSha = event.payload.head_commit.tree_id;
-    await CreateCommitStatus(github, repoOwner, repoName, headSha, "pending", "The process has started.");
+    await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "pending", "The process has started.");
     if (!TX_ALL_UPDATE) {
         const addModResources = AddModResources(event.payload.commits);
-        await UpdateResources(github, repoOwner, repoName, addModResources)
+        await UpdateResources(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, addModResources)
             .catch(async () => {
                 const message = "Failed to upload to Transifex.";
-                await CreateCommitStatus(github, repoOwner, repoName, headSha, "failure", message);
+                await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "failure", message);
                 logger.fatal(message);
                 throw new Error(message);
             });
     } else {
-        await AllUpdateResources(github, repoOwner, repoName, headTreeSha)
+        await AllUpdateResources(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headTreeSha)
             .catch(async () => {
                 const message = "Failed to upload all resource files to Transifex.";
-                await CreateCommitStatus(github, repoOwner, repoName, headSha, "failure", message);
+                await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "failure", message);
                 logger.fatal(message);
                 throw new Error(message);
             });
     }
-    const allResources = await AllResources(github, repoOwner, repoName, headTreeSha)
+    const allResources = await AllResources(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headTreeSha)
         .catch(async () => {
             const message = "Failed to acquire the path of the target file on GitHub.";
-            await CreateCommitStatus(github, repoOwner, repoName, headSha, "failure", message);
+            await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "failure", message);
             logger.fatal(message);
             throw new Error(message);
         });
     const allLanguages = await AllLanguages()
         .catch(async () => {
             const message = "Failed to get the list of languages to be translated from Transifex.";
-            await CreateCommitStatus(github, repoOwner, repoName, headSha, "failure", message);
+            await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "failure", message);
             logger.fatal(message);
             throw new Error(message);
         });
     const allTranslations = await AllTranslations(allResources, allLanguages)
         .catch(async () => {
             const message = "Failed to download the translation file on Transifex.";
-            await CreateCommitStatus(github, repoOwner, repoName, headSha, "failure", message);
+            await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "failure", message);
             logger.fatal(message);
             throw new Error(message);
         });
-    await CommitTranslations(github, repoOwner, repoName, headSha, headTreeSha, allTranslations)
+    await CommitTranslations(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, headTreeSha, allTranslations)
         .catch(async () => {
             const message = "Failed to commit the translation file to GitHub.";
-            await CreateCommitStatus(github, repoOwner, repoName, headSha, "failure", message);
+            await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "failure", message);
             logger.fatal(message);
             throw new Error(message);
         });
-    await CreateCommitStatus(github, repoOwner, repoName, headSha, "success", "All processes are completed.");
+    await CreateCommitStatus(github, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, headSha, "success", "All processes are completed.");
+});
+
+server.post(TX_WEBHOOK_PATH, async (req, res) => {
+    logger.info("process start by Transifex's Webhook");
+
+    const body = JSON.parse(req.body);
+    const resourceSlug = body.resource;
+    const lang = body.language;
+
+    logger.info("process verify Transifex's Webhook");
+    if (!VerifyTxWebhook(req, WEBHOOK_SECRET) || body.project != TX_PROJECT_SLUG || lang == TX_RESOURCE_LANG) {
+        res.status(400).send("Bad Request");
+        return;
+    }
+    logger.info("verified Webhook");
+
+    logger.info("process get resource file information from Transifex");
+    const resource = await txApi.resource(TX_PROJECT_SLUG, resourceSlug)
+        .catch(() => {
+            const message = "Failed to get resource file information from Transifex.";
+            logger.fatal(message);
+            res.status(500).send("Internal Server Error");
+            throw new Error(message);
+        });
+    logger.info("got resource file infomation");
+
+    if (!resource.data.name.match(fileFilter) || !resource.data.name.match(extFilter)) {
+        res.status(400).send("Bad Request");
+        return;
+    }
+
+    logger.info("process download the translation file on Transifex");
+    const translation = await txApi.translation(TX_PROJECT_SLUG, resourceSlug, lang)
+        .catch(() => {
+            const message = "Failed to download the translation file on Transifex.";
+            logger.fatal(message);
+            res.status(500).send("Internal Server Error");
+            throw new Error(message);
+        });
+    logger.info("downloaded the translation file");
+
+    const translationPath = resource.data.name.replace(fileFilter, TX_TARGET_PATH.replace(new RegExp("<lang>", "g"), lang));
+    const github = await app.asInstallation(GITHUB_INSTALL_ID);
+
+    logger.info("process get information on translation file from GitHub");
+    const content = await github.repos.getContents({
+        owner: GITHUB_REPO_OWNER,
+        repo: GITHUB_REPO_NAME,
+        path: translationPath,
+        ref: GITHUB_BRANCH
+    }).catch(() => {
+        const message = "Failed to get information on translation file from GitHub.";
+        logger.fatal(message);
+        res.status(500).send("Internal Server Error");
+        throw new Error(message);
+    });
+    logger.info("got information on translation file");
+
+    logger.info("process commit the translation file to GitHub");
+    await github.repos.updateFile({
+        owner: GITHUB_REPO_OWNER,
+        repo: GITHUB_REPO_NAME,
+        path: translationPath,
+        message: "Update translation from transifex",
+        content: Buffer.from(translation.data).toString("base64"),
+        sha: content.data.sha,
+        branch: GITHUB_BRANCH
+    }).catch(() => {
+        const message = "Failed to commit the translation file to GitHub.";
+        logger.fatal(message);
+        res.status(500).send("Internal Server Error");
+        throw new Error(message);
+    });
+    logger.info("commited the translation file");
+
+    res.status(200).send("OK");
 });
 
 if (GITHUB_WEBHOOK_PROXY_URL) {
     createWebhookProxy(GITHUB_WEBHOOK_PROXY_URL, GITHUB_WEBHOOK_PATH);
-}
-
-if (TX_WEBHOOK_PROXY_URL) {
-    createWebhookProxy(TX_WEBHOOK_PROXY_URL, TX_WEBHOOK_PATH);
 }
 
 server.listen(PORT);
